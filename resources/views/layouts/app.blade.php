fff071aa9e22dfa299dcf12bf79104c934f7cd3b
3A832057 <qoooppadsl@gmail.com>
2021-11-20 01:09:55 +0800

    子樣板(tasks)、主樣板(app)的新增

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,1 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel 快速入門 - 基本</title>
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
